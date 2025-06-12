87c73f68cd588f65b3bfb13b3cbc2597ffa2e236
Jeremiah Menétrey <superjun1@gmail.com>
2025-06-12 23:29:04 +0200

    Simplify function to make it more robust and less surprising
    The fallback to counting from the function's input argument if an error occured
    while processing the formula made it difficult to be sure that it was computing
    what was expected. By only supporting the formula-based use-case, it always
    either does what is expected or fails visibly, giving the user the opportunity
    to identify the issue and fix it.
    
    The function doesn't support counting from its argument directly, but it's not
    something I use, and not something I expect to be commonly needed.
    
    The function now also indicates the faulty range ref in case of error resolving
    it.

--- src/bindings.js
+++ src/bindings.js
@@ -1,14 +1,12 @@
 /**
- * Sum up the number of volumes of the argument, a cell, a range or a list of
- * ranges.
+ * Sum up the number of volumes from a range.
  *
  * Examples:
  *
- *   =COUNTVOLUMES("1-3, 6, 8-10")
- *   =COUNTVOLUMES(C23)
- *   =COUNTVOLUMES(C2:C55)
- *   =COUNTVOLUMES(Comics!C:C)
- *   =COUNTVOLUMES(French!C:C;English!C:C)
+ *     =COUNTVOLUMES(C23)
+ *     =COUNTVOLUMES(C2:C55)
+ *     =COUNTVOLUMES(Comics!C:C)
+ *     =COUNTVOLUMES(French!C:C;English!C:C)
  *
  * Header rows and columns are automatically excluded if the start of the range
  * is unbounded (e.g. `C:C55`) and the rows/columns are frozen.
@@ -18,33 +16,32 @@
  * or column that contains data. This is a bit finicky, and might not always
  * work, in particular with nested functions.
  */
-function countVolumes(input) {
-  // When specified as a range, the `input` parameter contains the resolved
-  // values. For range with an unbounded end, this goes to the end of the
-  // document and not the end of the data.
-  // We can't easily discriminate between a value, a range or a nested formula,
-  // so we just try assuming it's a range, and if there is an exception we fall
-  // back to using the resolved values.
-  try {
-    var formula = SpreadsheetApp.getActiveRange().getFormula()
-    // There is always a formula (since COUNTVOLUMES was called), but it might
-    // not be a simple COUNTVOLUMES call, or not one with a simple range.
-    var refs = formula.match(/=\w+\((.*)\)/i)[1]
+function countVolumes() {
+  // We can't use the argument passed to the function when called from the macro
+  // because it contains the actual values resolved from the range. This does
+  // allow to detect whether the range is unbounded on either side. So instead
+  // we have to parse the formula to get the range specification and work it
+  // from there.
 
-    var values = refs.split(';')
-      .map(ref => (ref.includes('!')
-        ? SpreadsheetApp.getActive()
-        : SpreadsheetApp.getActiveSheet()
-      ).getRange(ref))
-      .map(range => clampRange(range))
-      .map(range => range.getValues())
-      .flat(Infinity)
+  var formula = SpreadsheetApp.getActiveRange().getFormula()
+  // There is always a formula (since COUNTVOLUMES was called), but it might
+  // not be a simple COUNTVOLUMES call, or not one with a simple range.
+  var refs = formula.match(/=.*COUNTVOLUMES\s*\(([^)]*)\)/i)[1]
 
-    return count(values)
-  }
-  catch (e) {
-    return (input.flat) ? count(input.flat(Infinity)) : count([input])
-  }
+  var values = refs.split(';')
+    .map(ref => {
+      try {
+        return SpreadsheetApp.getActive().getRange(ref)
+      } catch (e) {
+        e.message = `${e.message} (${ref})`
+        throw e
+      }
+    })
+    .map(range => clampRange(range))
+    .map(range => range.getValues())
+    .flat(Infinity)
+
+  return count(values)
 }
 
 
